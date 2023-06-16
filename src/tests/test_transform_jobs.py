501d671a08f84dc18dddd2d5b6ff3a49418de5d6
Alexandre Bernard <alexandre.bernard@epita.fr>
2023-06-16 21:17:38 +0200

    Job patching (#4)
    * implement job patching inside workflows
    
    Signed-off-by: Alexandre Bernard <alexandre.bernard@epita.fr>
    
    * update command-test
    
    Signed-off-by: Alexandre Bernard <alexandre.bernard@epita.fr>
    
    ---------
    
    Signed-off-by: Alexandre Bernard <alexandre.bernard@epita.fr>

---
+++ src/tests/test_transform_jobs.py
@@ -0,0 +1,62 @@
+import os
+from unittest import TestCase, mock, main
+
+from scripts import patch_workflows_jobs
+
+def build_params(input_config_path: str, output_config_path: str, job_names: list[str]) -> dict:
+    return {
+        "CIRCLECI_CONFIG_PATH": input_config_path,
+        "OUTPUT_CONFIG_PATH": output_config_path,
+        "JOBS_LIST": ','.join(job_names)
+    }
+
+class TestMain(TestCase):
+    @mock.patch.dict(os.environ, build_params(
+        os.path.join(os.path.dirname(__file__), "./assets/configs/config.yml"),
+        "/tmp/test_main.yml",
+        ["failing-job"]))
+    def test_main(self):
+        """
+        Test that it can find and path job in simple workflow
+        """
+        patch_workflows_jobs.main()
+        self.maxDiff = None
+        self.assertEqual(open("/tmp/test_main.yml", "r").read(),
+        """jobs:
+  failing-job:
+    docker:
+    - image: cimg/base
+    steps:
+    - run: exit 1
+  succeeding-job:
+    docker:
+    - image: cimg/base
+    steps:
+    - run: exit 0
+version: 2.1
+workflows:
+  fail:
+    jobs:
+    - context: global
+      failing-job: null
+      type: approval
+    - context: global
+      failing-job: null
+      type: approval
+  run-succeed-and-fail:
+    jobs:
+    - succeeding-job
+    - context: global
+      failing-job: null
+      requires:
+      - succeeding-job
+      type: approval
+  succeed:
+    jobs:
+    - succeeding-job
+  version: 2
+""")
+
+if __name__ == '__main__':
+    main()
+
