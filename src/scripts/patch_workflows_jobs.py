253b9597024e422a97b5a756df1dd894d5c058c0
Alexandre Bernard <alexandre.bernard@epita.fr>
2023-07-08 15:49:40 +0200

    wip
    Signed-off-by: Alexandre Bernard <alexandre.bernard@epita.fr>

--- src/scripts/patch_workflows_jobs.py
+++ src/scripts/patch_workflows_jobs.py
@@ -2,9 +2,15 @@
 
 import os
 import yaml
-from typing import List, Any, Union
+from typing import List, Union
 
 SKIPED_WORKFLOW_NAME = "skiped-jobs"
+
+class JobConfig:
+    def __init__(self, job_name: str, succeed_by_default: bool):
+        self.job_name = job_name
+        self.succeed_by_default = succeed_by_default
+
 
 """
 Returns:
@@ -25,15 +31,33 @@ Returns:
 Raise:
     An exception if the environment variable is unset or empty
 """
-def jobs_to_update() -> List[str]:
+def jobs_to_update() -> List[JobConfig]:
+    out: List[JobConfig] = []
+
     config_path = get_mandatory_env('JOBS_TO_TRANSFORM_CONFIG_PATH')
-    print(f'opening {config_path}');
+    print(f'opening {config_path}')
+
+    print(f'opening {config_path}')
     with open(config_path, 'r') as config_file:
         print(f'parsing {config_file}')
         config_yaml = yaml.safe_load(config_file)
-        jobs = config_yaml["jobs"]
 
-        return jobs.keys()
+    jobs = config_yaml["jobs"]
+    if not isinstance(jobs, dict):
+        raise Exception("expected `jobs` to be a dictionary")
+
+    job_names = jobs.keys()
+    for job_name in job_names:
+        job: dict = jobs[job_name]
+        if not isinstance(job, dict):
+            raise Exception("expected element in `jobs` to be a dictionary")
+
+        out.append(JobConfig(
+            job_name=job_name,
+            succeed_by_default=job.get('succeed-by-default', False)))
+
+    return out
+
 
 def as_matching_job(job_value: Union[str, dict], searched_name: str) -> Union[dict, None]:
     if isinstance(job_value, str):
@@ -49,7 +73,7 @@ def as_matching_job(job_value: Union[str, dict], searched_name: str) -> Union[di
 Mutates the `workflows` attribute in the given config_yaml so that every workflow
 which is referencing the given job now makes it wait for a global approval
 """
-def move_job_to_skiped_workflow(config_yaml: dict, job_name: str):
+def move_job_to_skiped_workflow(config_yaml: dict, job_config: JobConfig):
     workflows = config_yaml["workflows"]
     for workflow_name, workflow_object in workflows.items():
         if not isinstance(workflow_object, dict):
@@ -59,14 +83,14 @@ def move_job_to_skiped_workflow(config_yaml: dict, job_name: str):
         # Find the job
         workflow_jobs: list[dict] = workflow_object["jobs"]
         for idx, job_object in enumerate(workflow_jobs):
-            job_object = as_matching_job(job_object, job_name)
+            job_object = as_matching_job(job_object, job_config.job_name)
             if job_object is None:
                 continue
 
             # Update its body so that it waits for an approval
-            print(f"making job {job_name} wait for an approval in workflow {workflow_name}")
-            del job_object[job_name]
-            workflow_jobs[idx] = { job_name: { **job_object, "context": "global", "type": "approval" } }
+            print(f"making job {job_config} wait for an approval in workflow {workflow_name}")
+            del job_object[job_config]
+            workflow_jobs[idx] = { job_config: { **job_object, "context": "global", "type": "approval" } }
 
 """
 Description:
@@ -76,14 +100,14 @@ Parameters:
 Raise:
     An exception when a job is not present in the given config
 """
-def update_config(config_yaml: dict, jobs_to_transform: List[str]):
+def update_config(config_yaml: dict, jobs_to_transform: List[JobConfig]):
     jobs_object = config_yaml["jobs"]
     if jobs_object is None:
         print(f"config object: \n{config_yaml}")
         raise Exception('expected the config to have a "jobs" object at top level')
 
-    for job_name in jobs_to_transform:
-        move_job_to_skiped_workflow(config_yaml, job_name)
+    for job_config in jobs_to_transform:
+        move_job_to_skiped_workflow(config_yaml, job_config)
 
 def main():
     jobs = jobs_to_update()
@@ -92,8 +116,6 @@ def main():
     config_yaml = {}
 
     with open(input_config_path, 'r') as config_file:
-        print(f'parsing {input_config_path}')
-        config_yaml = yaml.safe_load(config_file)
         print(f'updating the {jobs} so that they wait for approval')
         update_config(config_yaml, jobs)
 
